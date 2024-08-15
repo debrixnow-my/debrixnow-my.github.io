4de510db5387152f17c6e065978e8c40b06d30f0
debrixnow-my <contact.kael911@gmail.com>
2024-08-15 16:23:58 +0800

    Update script.js

--- kodam/script.js
+++ kodam/script.js
@@ -1,6 +1,7 @@
 document.getElementById("submitBtn").addEventListener("click", function() {
     const nameInput = document.getElementById("nameInput").value;
-    const randomWords = ["Sakti", "Berani", "Bijaksana", "Setia", "Cerdas", "Tangguh"];
+    const randomWords = ["gondoruwo", "tutup odol", "buaya brazil", "Setia", "samsudin", "miya hyper" "Topi Melorot", "Pocong Bersepeda", "Batu Bata", "suster mesok", "masako", "panji petualang", "kopi tuak", "miya Khalifah", "niroro gundul", "mak lampir", "megawati", 
+"ibu puan + tek"];
     const randomIndex = Math.floor(Math.random() * randomWords.length);
 
     if (/^[A-Za-z\s]+$/.test(nameInput) && nameInput !== "") {
